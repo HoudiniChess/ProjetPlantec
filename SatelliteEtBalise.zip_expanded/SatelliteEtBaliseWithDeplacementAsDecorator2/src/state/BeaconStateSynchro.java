10e5153fc43602945f8b312ee776c29c29d99f64
Houdini <yannlebris97@gmail.com>
2021-01-22 01:59:11 +0100

    j'ai oublié ça, oupsi

---
+++ SatelliteEtBalise.zip_expanded/SatelliteEtBaliseWithDeplacementAsDecorator2/src/state/BeaconStateSynchro.java
@@ -0,0 +1,73 @@
+package state;
+
+import events.SatelliteMoveListener;
+import events.SatelliteMoved;
+import events.SynchroEvent;
+import model.Beacon;
+import model.Satellite;
+
+public class BeaconStateSynchro extends BeaconState implements SatelliteMoveListener
+{
+
+  protected final static int SYNCHROTIME = 20;
+  protected Satellite satSynchro;
+  protected Beacon beacon;
+
+  @Override
+  public void install(Beacon beacon)
+  {
+    this.beacon = beacon;
+    beacon.getManager().beaconReadyForSynchro(this);
+  }
+
+  @Override
+  public void behaviorState(Beacon beacon)
+  {
+    if (!this.synchroStarted())
+    {
+      return;
+    }
+
+    beacon.getMemory().removeData(SYNCHROTIME);
+
+    if (beacon.getMemory().getFillMemory() == 0)
+    {
+      Satellite sat = this.satSynchro;
+      this.satSynchro = null;
+      beacon.send(new SynchroEvent(this));
+      sat.send(new SynchroEvent(this));
+      beacon.getManager().beaconSynchroDone(this);
+      this.nextState(beacon);
+    }
+  }
+
+  @Override
+  public void nextState(Beacon beacon)
+  {
+    BeaconStateDiving stateDiving = new BeaconStateDiving();
+    stateDiving.install(beacon);
+    beacon.setState(stateDiving);
+  }
+
+  public Boolean synchroStarted()
+  {
+    return this.satSynchro != null;
+  }
+
+  @Override
+  public void whenSatelliteMoved(SatelliteMoved arg)
+  {
+    if (this.synchroStarted())
+      return;
+    Satellite sat = (Satellite) arg.getSource();
+    int satX = sat.getPosition().x;
+    int tarX = beacon.getPosition().x;
+    if (satX > tarX - 10 && satX < tarX + 10)
+    {
+      this.satSynchro = sat;
+      beacon.send(new SynchroEvent(this));
+      this.satSynchro.send(new SynchroEvent(this));
+    }
+  }
+
+}
